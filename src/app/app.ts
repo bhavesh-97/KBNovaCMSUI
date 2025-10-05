c5248e542da859567e04cf3d962755137c8e0c4e
Bhavesh MotiBhai Vaghela <vaghelabhaveshm3@gmail.com>
2025-10-05 22:51:17 +0530

    Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 @Component({
@@ -7,6 +7,24 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('KBNovaCMSUI');
+
+  ngOnInit(): void {
+    // Call setFavicon on initialization
+    this.setFavicon('./assets/images/product/KBNova.png');
+  }
+
+  setFavicon(iconUrl: string) {
+    let link: HTMLLinkElement | null = document.querySelector("link[rel*='icon']");
+    
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    
+    link.type = 'image/x-icon';
+    link.href = iconUrl;
+  }
 }
